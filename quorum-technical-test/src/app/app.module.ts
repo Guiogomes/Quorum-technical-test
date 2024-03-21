b8b6c4d35f721c79654b7d8300b59227daf07e18
Guilherme Gomes <ggs.arquiteto@gmail.com>
2024-03-20 23:36:29 -0300

    :construction: add some imports to app module file

--- quorum-technical-test/src/app/app.module.ts
+++ quorum-technical-test/src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ɵBrowserAnimationBuilder } from '@angular/animations';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    FormsModule,
+    CommonModule,
+    BrowserModule,
+    RouterModule,
+    MatTabsModule,
+  ],
+
 })
 export class AppModule { }
