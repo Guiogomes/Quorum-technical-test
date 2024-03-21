f7d89f6d2bf36b4eac7b93386e62fc07ae53cbff
Guilherme Gomes <ggs.arquiteto@gmail.com>
2024-03-21 00:31:06 -0300

    :construction: implements autosave on visual studio code and save files it's not saved

--- quorum-technical-test/src/app/app-routing.module.ts
+++ quorum-technical-test/src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { BillByLegislatorViewComponent } from './modules/view-bills-by-legislator/view/bill-by-legislator-view/bill-by-legislator-view.component';
 
 
+const routes: Routes = [
+  {
+  path: 'bills-by-legislator',
+  component: BillByLegislatorViewComponent,
+  pathMatch: 'full'
+}
+  
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
   ]
 })
 export class AppRoutingModule { }
--- quorum-technical-test/src/app/app.component.ts
+++ quorum-technical-test/src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+/* eslint-disable no-bitwise */
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
+/** Class for the app component that is bootstrapped to run the application
+ */
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'body',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'quorum-technical-test';
+export class AppComponent implements OnInit {
+	/** string used to hold the url for the skipToMain link */
+	skipToMain: string | undefined;
+	users = [];
+
+	/** constructor for setting up DI in this component */
+	constructor(
+		private readonly router: Router,
+	) {}
+
+	/** this class requires this method to implement the OnInit interface */
+	ngOnInit() {
+		this.router.events
+			.pipe(filter((event) => event instanceof NavigationEnd))
+			.subscribe((event: NavigationEnd) => {
+				this.setSkipLinkUrl(event.urlAfterRedirects);
+			});
+	}
+
+	/**
+	 * setSkipLinkUrl takes in a url string and processes whether
+	 * the skipToMain link should be updated to use the new value
+	 * @param currentUrl the new url to refer to
+	 */
+	private setSkipLinkUrl(currentUrl: string): void {
+		if (!currentUrl.endsWith('#app-content')) {
+			this.skipToMain = `${currentUrl}#app-content`;
+		}
+	}
 }
