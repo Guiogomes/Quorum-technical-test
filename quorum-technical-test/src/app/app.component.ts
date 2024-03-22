0ce67c05e1f934baa3905a87e875f05b1e3009f4
Guilherme Gomes <ggs.arquiteto@gmail.com>
2024-03-22 00:46:03 -0300

    :construction: some tests for rendering component

--- quorum-technical-test/src/app/app.component.ts
+++ quorum-technical-test/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BillByLegislatorViewComponent } from './modules/view-bills-by-legislator/view/bill-by-legislator-view/bill-by-legislator-view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BillByLegislatorViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- quorum-technical-test/src/app/app.module.ts
+++ quorum-technical-test/src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ɵBrowserAnimationBuilder } from '@angular/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BillByLegislatorViewComponent } from './modules/view-bills-by-legislator/view/bill-by-legislator-view/bill-by-legislator-view.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BillByLegislatorViewComponent
   ],
   imports: [
     FormsModule,
@@ -19,6 +22,7 @@ import { ɵBrowserAnimationBuilder } from '@angular/animations';
     RouterModule,
     MatTabsModule,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 
 })
 export class AppModule { }
