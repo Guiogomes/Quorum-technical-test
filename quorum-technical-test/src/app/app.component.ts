13e0acc3c84031c2a882598f1fdbf7aa7af55094
Guilherme Gomes <ggs.arquiteto@gmail.com>
2024-03-22 21:16:28 -0300

    :construction: build ts file

--- quorum-technical-test/src/app/app.component.ts
+++ quorum-technical-test/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CsvSelectorService } from './services/csv-selector.service';
 import { RouterOutlet } from '@angular/router';
 import { BillByLegislatorViewComponent } from './modules/view-bills-by-legislator/view/bill-by-legislator-view/bill-by-legislator-view.component';
@@ -6,31 +6,74 @@ import { ICsvData } from './Interfaces/ICsvData';
 import { ICsvRows } from './Interfaces/ICsvRows';
 import { map, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { OnChanges } from '@angular/core';
+import { SimpleChange } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+interface LegislatorVotes {
+  legislator_id: number;
+  legislator_name: string;
+  bills_supported: number;
+  bills_oppose: number;
+}
+
+interface BillsVotes {
+  bill_id: number;
+  bill_title: string;
+  sponsor_id: number;
+  support_votes: number;
+  oppose_votes: number;
+}
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit{
-  title = 'quorum-technical-test';
-  csvData: ICsvData = { headers: [], rows: [] };;
-  csvUrl: string = '';
+  title = 'Quorum Technical Test';
+  legislatorCsvData: ICsvData | undefined = { headers: [], rows: []};
+  billsCsvData: ICsvData | undefined = { headers: [], rows: []};
+  votesResultCsvData: ICsvData | undefined  = { headers: [], rows: []};
+  votesCsvData: ICsvData | undefined = { headers: [], rows: []};
+  legislatorsVotesInfos: LegislatorVotes[] | [] = [];
 
-  constructor(private csvService: CsvSelectorService) {
+  constructor(private csvService: CsvSelectorService) { }
+  
+  async ngOnInit(): Promise<void> {
+      await this.retrieveCsvFileInformation('../assets/legislators_(2).csv', 'legislator');
+      await this.retrieveCsvFileInformation('../assets/bills_(2).csv', 'bills');
+      await this.retrieveCsvFileInformation('../assets/vote_results_(2).csv', 'votesResults');
+      await this.retrieveCsvFileInformation('../assets/votes_(2).csv', 'votes');
+      this.filterVotesPerLegislators();
   }
   
-  ngOnInit(): void {
-    this.retrieveCsvFileInformation();
-    console.log(this.csvData);
+  async retrieveCsvFileInformation(fileUrl: string, csvAttributeName: string): Promise<void> {
+    const data = await this.csvService.getCsvData(fileUrl);
+    switch(csvAttributeName.toLowerCase()) {
+        case 'legislator':
+          this.legislatorCsvData = data;
+          break
+        case 'bills':
+          this.billsCsvData = data;
+          break
+        case 'voteResults':
+          this.votesResultCsvData = data;
+          break
+        case 'votes':
+          this.votesCsvData = data
+          break
+      }
+    ;
   }
-  
-  retrieveCsvFileInformation() {
-    this.csvUrl = '../../../../../assets/bills_(2).csv'
-    this.csvService.getCsvData(this.csvUrl).subscribe(data => {
-      this.csvData = data;
-    });
+
+  filterVotesPerLegislators() {
+    if(!this.legislatorCsvData || !this.votesResultCsvData) return;
+    
   }
 }
